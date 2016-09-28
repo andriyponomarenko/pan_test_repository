c99de08e505d6fb8617a69b48ff7b4c97b547d18
Andriy Ponomarenko <andriy.ponomarenko@globallogic.com>
2016-09-28 11:43:58 +0300

    no message

---
+++ test01.ps1
@@ -0,0 +1,11 @@
+write-host "Hello world!"
+
+write-host "Hello2 world!"
+
+write-host "Hello3 world!"
+
+write-host "Hello4 world! for test_branch_2"
+write-host "Hello4 world! one more for test_branch_2"
+write-host "Hello4 world! for test_branch_1"
+
+write-host "try to commit in main branch2"
